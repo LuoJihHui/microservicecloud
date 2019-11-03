3ef309dfc142146e14250129ceb0e2e52dad4296
Admin <2197813911@qq.com>
2019-11-03 20:23:52 +0800

    添加子模块

--- microservicecloud-api/src/main/java/com/example/ljh/microservicecloudapi/entities/Dept.java
+++ microservicecloud-api/src/main/java/com/example/ljh/microservicecloudapi/entities/Dept.java
@@ -4,17 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 
 /**
  * 部门信息表
- *
+ * @Accessors(chain = true) 链式操作
  * @author luojihui
  * @date 2019/11/3 20:16
  */
 @Data
 @Builder
+@Accessors(chain = true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class Dept implements Serializable {
